4f66338fe55a70135c5c9bee5b19d51e4d54f59c
DESKTOP-FH5BK1A\chsan <chsandeep79@gmail.com>
2023-11-01 16:12:29 -0500

    Git ignore files added

---
+++ Project12_HMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project12_HMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
